55ba100d720faa023f68c16cae89cdd6294fbc40
JullyanoBortolonSchuhmacher <jullyano_schuhmac@estudante.sesisenai.org.br>
2024-07-27 23:18:24 -0300

    Criando a tabela/migration dos usuarios # vai ser adicionado: - criptografia - rotas - criar / atualizar / deletar / ler com os métodos do controller
    _j
    -( 1 )

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,11 +1,18 @@
-const { config } = require('dotenv')
-config()
+require('dotenv').config();
 
 module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
-}
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  }
+};
---
+++ src/database/seeders/20240728020208-seed-usuarios.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('usuarios', [
+      {
+        nome: 'João Silva',
+        sexo: 'Masculino',
+        CPF: '123.456.789-01',
+        endereco: JSON.stringify({
+          rua: 'Rua A',
+          numero: '123',
+          bairro: 'Centro',
+          cidade: 'Cidade X',
+          estado: 'BA',
+          cep: '12345-678'
+        }),
+        email: 'joao.silva@exemplo.com',
+        senha: '12345678',
+        dataNascimento: '01/08/1994',
+        criadoEm: new Date(),
+      },
+      {
+        nome: 'Maria Souza',
+        sexo: 'Feminino',
+        CPF: '987.654.321-00',
+        endereco: JSON.stringify({
+          rua: 'Avenida B',
+          numero: '456',
+          bairro: 'Bairro Z',
+          cidade: 'Cidade Y',
+          estado: 'RR',
+          cep: '87654-321'
+        }),
+        email: 'maria.souza@exemplo.com',
+        senha: 'sucodebanana',
+        dataNascimento: '02/02/1992',
+        criadoEm: new Date(),
+      },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('usuarios', null, {});
+  }
+};
